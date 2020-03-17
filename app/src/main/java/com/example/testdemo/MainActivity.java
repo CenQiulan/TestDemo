10ae62bef75dff3ae9d15f693ff934e4269d057f
lenovo <cenqiulan@foxmail.com>
2020-03-17 18:34:42 +0800

    2

--- app/src/main/java/com/example/testdemo/MainActivity.java
+++ app/src/main/java/com/example/testdemo/MainActivity.java
@@ -2,21 +2,18 @@ package com.example.testdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
-import android.view.InflateException;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -54,40 +51,47 @@ public class MainActivity extends AppCompatActivity {
             "20175980",
             "20175990"
     };
+    private int []stuImg={
+            R.mipmap.stu_1, R.mipmap.stu_2,R.mipmap.stu_3,R.mipmap.stu_4,R.mipmap.stu_5,
+            R.mipmap.stu_6,R.mipmap.stu_7,R.mipmap.stu_8,R.mipmap.stu_9,R.mipmap.stu_10,
+            R.mipmap.stu_11,R.mipmap.stu_12,R.mipmap.stu_13,R.mipmap.stu_14,R.mipmap.stu_15,
+            R.mipmap.stu_16,R.mipmap.stu_17,R.mipmap.stu_18,R.mipmap.stu_19,R.mipmap.stu_20,
+            R.mipmap.stu_21,R.mipmap.stu_22,R.mipmap.stu_23,R.mipmap.stu_24,R.mipmap.stu_25,
+            R.mipmap.stu_26,R.mipmap.stu_27,R.mipmap.stu_28,R.mipmap.stu_29,R.mipmap.stu_30,
+            R.mipmap.stu_31,R.mipmap.stu_32};
     private ListView listView;
+    ColorMatrixColorFilter filter ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         listView = (ListView) findViewById(R.id.list_view);
-/*        MyAdaptor myAdaptor=new MyAdaptor();
-        listView.setAdapter(myAdaptor);*/
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.listitem, R.id.list_item, stuId);
-        listView.setAdapter(arrayAdapter);
+
+        ColorMatrix colorMatrix=new ColorMatrix();
+        colorMatrix.setSaturation(0);
+        filter=new ColorMatrixColorFilter(colorMatrix);
+
+        MyAdapter myAdapter=new MyAdapter();
+        listView.setAdapter(myAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                Intent intent=new Intent(MainActivity.this,ShowInfo.class);
+                intent.putExtra("stuId",stuId[position]);
+                intent.putExtra("stu_position",position);
+                startActivity(intent);
+                finish();
             }
         });
     }
-/*        List<Map<String, String>> data=new ArrayList<>();
-        for(int i=0;i<name.length;i++){
-            Map<String,String>item=new HashMap<>();
-            item.put("name",name[i]);
-            data.add(item);
-        }
-        SimpleAdapter simpleAdapter=new SimpleAdapter(this,data,R.layout.listitem,new String[]{"name"},new int[]{R.id.list_item});
-        listView.setAdapter(simpleAdapter);
-    }*/
-    //give data to listview
-  /*  private class MyAdaptor extends BaseAdapter {
 
+    private class MyAdapter extends BaseAdapter {
 
         @Override
         public int getCount() {
-            return name.length;
+            return stuImg.length;
         }
 
         @Override
@@ -102,13 +106,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            System.out.println("position:" + position);
-
             View view = getLayoutInflater().inflate(R.layout.listitem, null);
-            TextView textView = (TextView) view.findViewById(R.id.list_item);
-            textView.append(name[position]);
+            ImageView imgV =view.findViewById(R.id.item_img);
+            TextView textView=view.findViewById(R.id.item_text);
 
+            imgV.setImageResource(stuImg[position]);
+            imgV.setColorFilter(filter);
+            textView.setText(stuId[position]);
             return view;
+        }
     }
-    }*/
 }
--- app/src/main/java/com/example/testdemo/ShowInfo.java
+++ app/src/main/java/com/example/testdemo/ShowInfo.java
@@ -2,13 +2,44 @@ package com.example.testdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ShowInfo extends AppCompatActivity {
-
+    private int []stuImg={
+            R.mipmap.stu_1, R.mipmap.stu_2,R.mipmap.stu_3,R.mipmap.stu_4,R.mipmap.stu_5,
+            R.mipmap.stu_6,R.mipmap.stu_7,R.mipmap.stu_8,R.mipmap.stu_9,R.mipmap.stu_10,
+            R.mipmap.stu_11,R.mipmap.stu_12,R.mipmap.stu_13,R.mipmap.stu_14,R.mipmap.stu_15,
+            R.mipmap.stu_16,R.mipmap.stu_17,R.mipmap.stu_18,R.mipmap.stu_19,R.mipmap.stu_20,
+            R.mipmap.stu_21,R.mipmap.stu_22,R.mipmap.stu_23,R.mipmap.stu_24,R.mipmap.stu_25,
+            R.mipmap.stu_26,R.mipmap.stu_27,R.mipmap.stu_28,R.mipmap.stu_29,R.mipmap.stu_30,
+            R.mipmap.stu_31,R.mipmap.stu_32};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_info);
+        Intent intent=getIntent();
+        String string=intent.getStringExtra("stuId");
+        int position=intent.getIntExtra("stu_position",0);
+        ImageView imageView=findViewById(R.id.show_img);
+        imageView.setImageResource(stuImg[position]);
+
+        TextView textView=findViewById(R.id.show_info);
+        textView.setText("the student's id is:"+string);
+
+        Button button=findViewById(R.id.btn_return);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ShowInfo.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
